59b3416f5dda0e035ff6aa2f986ee539babb20ae
Nishant <bindhaninishant@gmail.com>
2024-10-16 14:19:25 +0530

    Basic Backend CRUD controllers are set

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,26 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 
+const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username })
+      .select("-password")
+      .select("-updatedAt");
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: "User Not found for Viewing Profile" });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+    console.log("Error in gettingUserProfile: ", error.message);
+  }
+};
+
 //signup user
 const signupUser = async (req, res) => {
   try {
@@ -85,7 +105,7 @@ const followUnfollowUser = async (req, res) => {
     const userToModify = await User.findById(id);
     const currentUser = await User.findById(req.user._id);
 
-    if (id == req.user._id) {
+    if (id == req.user._id.toString()) {
       return res
         .status(400)
         .json({ message: "You cannot follow/unfollow yourself" });
@@ -107,11 +127,53 @@ const followUnfollowUser = async (req, res) => {
       //follow user
       await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
       await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
-      res.status(200).json({ message: "User followed Successfully" });
+      res.status(200).json({ message: "User is followed Successfully" });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
     console.log(`Error in follow unfollow User`, error.message);
   }
 };
-export { signupUser, loginUser, logoutUser, followUnfollowUser };
+
+const updateUser = async (req, res) => {
+  const { name, email, username, password, profilePic, bio } = req.body;
+  const userId = req.user._id;
+  try {
+    let user = await User.findById(userId);
+    if (!user)
+      return res.status(400).json({ message: "User Not found for Updation" });
+
+    if (req.params.id !== userId.toString())
+      return res
+        .status(400)
+        .json({ message: "You cannot update other user profile" });
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    user = await user.save();
+
+    res.status(200).json({ message: "Profile Updated Successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log(`Error in updating User`, error.message);
+  }
+};
+
+export {
+  signupUser,
+  loginUser,
+  logoutUser,
+  followUnfollowUser,
+  updateUser,
+  getUserProfile,
+};

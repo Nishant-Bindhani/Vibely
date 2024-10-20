69b395b545d6bee7d8aaca5c2ae7813e12d74c6c
Nishant <bindhaninishant@gmail.com>
2024-10-21 01:37:25 +0530

    Adding reply done

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -127,7 +127,7 @@ const replyToPost = async (req, res) => {
     post.replies.push(reply);
     await post.save();
 
-    res.status(200).json({ message: "Replied Added Successfully", post });
+    res.status(200).json(reply);
   } catch (err) {
     res.status(500).json({ error: err.message });
     console.log("err in replying to post", err.message);

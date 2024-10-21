bbb0d78bd765ef0de1f8e0195c39d4cced21d849
Nishant <bindhaninishant@gmail.com>
2024-10-21 22:01:15 +0530

    custom hook for fetching user , post page updated , replies section created

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -55,7 +55,7 @@ const getPost = async (req, res) => {
     if (!post) {
       return res.status(404).json({ error: "Post not Found" });
     }
-    res.status(200).json({ post });
+    res.status(200).json(post);
   } catch (err) {
     res.status(500).json({ error: err.message });
     console.log("err in getting post", err.message);
